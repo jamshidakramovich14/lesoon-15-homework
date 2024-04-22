4dfa13e77e62bb86b81ee1ef1825d81130bd1f07
jamshidakramovich14 <jamshidturdiyev761@gmail.com>
2024-04-22 21:38:39 +0500

    changed

--- script.js
+++ script.js
@@ -1,2 +1,82 @@
 "use strict"
 
+
+// -------------------- 1-masala start --------------------
+
+// let bookshelf = [];
+
+// function addBook(title, author, genre, pages, isRead) {
+//   const book = {
+//     title,
+//     author,
+//     genre,
+//     pages,
+//     isRead,
+//   };
+
+//   bookshelf.push(book);
+// }
+
+// function markBookAsRead(bookIndex) {
+//   const book = bookshelf[bookIndex];
+
+//   book.isRead = true;
+// }
+
+// addBook("The Great Gatsby", "F. Scott Fitzgerald", "Classic", 180, false);
+// addBook("To Kill a Mockingbird", "Harper Lee", "Fiction", 281, true);
+
+// console.log(bookshelf);
+
+// markBookAsRead(0);
+
+// console.log(bookshelf);
+
+// -------------------- 1-masala end --------------------
+
+
+
+
+
+
+
+// -------------------- 2-masala start --------------------
+
+// let contactList = [];
+
+// function addContact(name, email, phone, address) {
+//   const contact = {
+//     name,
+//     email,
+//     phone,
+//     address,
+//   };
+
+//   contactList.push(contact);
+// }
+
+// function updateContact(contactIndex, newEmail, newPhone) {
+//   const contact = contactList[contactIndex];
+
+//   const updatedContact = {
+//     ...contact,
+//     email: newEmail !== undefined ? newEmail : contact.email,
+//     phone: newPhone !== undefined ? newPhone : contact.phone,
+//   };
+
+//   contactList[contactIndex] = updatedContact;
+// }
+
+// addContact("John Doe", "jdoe@example.com", "1234567890", {
+//   street: "123 Main St",
+//   city: "New York",
+//   country: "USA",
+// });
+
+// console.log(contactList);
+
+// updateContact(0, "johndoe@example.com", "9876543210");
+
+// console.log(contactList);
+
+// -------------------- 2-masala end --------------------
